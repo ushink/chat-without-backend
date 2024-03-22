44c85db88519b6d7f8b59e429d88e5ac8c4f13c8
Ulyana <ushinkevich@mail.ru>
2024-03-22 14:54:14 +0400

    feat: middleware from store

--- src/store.js
+++ src/store.js
@@ -1,20 +1,53 @@
 import { create } from "zustand";
 
-const useStore = create((set, get) => ({
-  messages: [],
-  createMessage: ({ sender, text, time }) => {
-    const { messages } = get();
-    const newMessage = {
-      sender,
-      text,
-      time,
-    };
-    
-    // asyns await ???
-    set({
-      messages: [newMessage].concat(messages),
-    });
-  },
-}));
+// Использую Middleware чтобы перехватывать изменения и сохранять в localStorage
+const localStorageUpdate = (config) => (set, get, api) =>
+  config(
+    (nextState, ...args) => {
+      if (nextState) {
+        window.localStorage.setItem(
+          "messages",
+          JSON.stringify(nextState.messages)
+        );
+      }
+
+      set(nextState, ...args);
+    },
+    get,
+    api
+  );
+
+// Вытаскиваю сообщения при перезагрузке из localStorage
+const getCurrentState = () => {
+  try {
+    const currentState = JSON.parse(
+      window.localStorage.getItem("messages") || "[]"
+    );
+
+    return currentState;
+  } catch (error) {
+    window.localStorage.setItem("messages", "[]");
+  }
+  
+  return [];
+};
+
+const useStore = create(
+  localStorageUpdate((set, get) => ({
+    messages: getCurrentState(),
+    createMessage: ({ sender, text, time }) => {
+      const { messages } = get();
+      const newMessage = {
+        sender,
+        text,
+        time,
+      };
+
+      set({
+        messages: [newMessage].concat(messages),
+      });
+    },
+  }))
+);
 
 export default useStore;
