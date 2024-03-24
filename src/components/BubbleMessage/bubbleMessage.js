31a7404bb06a520413e301160605535d278da5b3
Ulyana <ushinkevich@mail.ru>
2024-03-24 14:09:14 +0400

    prog: bubble message add buttons

--- src/components/BubbleMessage/bubbleMessage.js
+++ src/components/BubbleMessage/bubbleMessage.js
@@ -2,6 +2,8 @@ import Image from "next/image";
 import "./bubbleMessage.css";
 import useStore from "@/src/store/store";
 import { useEffect, useRef, useState } from "react";
+import ButtonUpdate from "../buttonsMessage/ButtonUpdate/update";
+import ButtonDelete from "../buttonsMessage/ButtonDelete/delete";
 
 export default function BubbleMessage({
   isEditMode,
@@ -24,40 +26,63 @@ export default function BubbleMessage({
   }, [isEditMode]);
 
   return (
-    <div className={sender === "user" ? "chat__message" : "chat__message-bot"}>
-      <div className="chat__message-content">
-        {sender === "bot" && (
-          <div className="name">
-            Janet <span className="role">Bot</span>
-          </div>
-        )}
+    <div>
+      <div
+        className={sender === "user" ? "chat__message" : "chat__message-bot"}
+      >
+        <label className="chat__message-content">
+          {sender === "bot" && (
+            <div className="name">
+              Janet <span className="role">Bot</span>
+            </div>
+          )}
 
-        {isEditMode && sender === "user" ? (
-          <input
-            className="chat__message-input"
+          {isEditMode === id && sender === "user" ? (
+            <input
+              className="chat__message-input"
+              type="text"
+              value={value}
+              ref={editTextRef}
+              onChange={(e) => {
+                e.stopPropagation();
+                setValue(e.target.value);
+              }}
+              onKeyDown={(e) => {
+                if (e.key === "Enter") {
+                  updateMessage(id, value);
+                  setIsEditMode("");
+                }
+              }}
+            />
+          ) : (
+            <div className="chat__message-text">{text}</div>
+          )}
+        </label>
+
+        <div className="chat__message-time">
+          {time}
+          {sender === "user" && (
+            <Image
+              src="/read-receipt.svg"
+              width={16}
+              height={8}
+              alt="receipt"
+            />
+          )}
+        </div>
+      </div>
+
+      {sender === "user" && (
+        <div className="user-message__buttons">
+          <ButtonUpdate
+            isEditMode={isEditMode}
+            setIsEditMode={setIsEditMode}
+            id={id}
             value={value}
-            ref={editTextRef}
-            onChange={(e) => {
-              setValue(e.target.value);
-            }}
-            onKeyDown={(e) => {
-              if (e.key === "Enter") {
-                updateMessage(id, value);
-                setIsEditMode(false);
-              }
-            }}
           />
-        ) : (
-          <div className="chat__message-text">{text}</div>
-        )}
-      </div>
-
-      <div className="chat__message-time">
-        {time}
-        {sender === "user" && (
-          <Image src="/read-receipt.svg" width={16} height={8} alt="receipt" />
-        )}
-      </div>
+          <ButtonDelete id={id} />
+        </div>
+      )}
     </div>
   );
 }
